83077a69081399e6027d0824ee658f5357663488
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-02-28 12:09:50 +0100

    manage state in header

--- projects/layout/src/lib/header/header.component.ts
+++ projects/layout/src/lib/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ResponsiveService } from '../responsive.service';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { BehaviorSubject } from 'rxjs';
 
 export interface NavItem { label: string, route: string };
 
@@ -11,6 +12,8 @@ export interface NavItem { label: string, route: string };
 })
 export class HeaderComponent implements OnInit {
 
+  isMenuOpen = false;
+
   faBars = faBars;
 
   @Input() navItems: NavItem[] = [
@@ -18,8 +21,36 @@ export class HeaderComponent implements OnInit {
     { label: 'Menu2', route: '/menu2' },
   ];
 
-  constructor(public responsive: ResponsiveService) { }
+  private isOpen$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  constructor(public responsive: ResponsiveService) {
+    this.isOpen$.subscribe(isOpen => {
+      this.isMenuOpen = isOpen;
+      this.manageBody();
+      console.log('isOpen', this.isMenuOpen);
+    });
+  }
 
   ngOnInit() { }
+
+  toggleMenu() {
+    this.isOpen$.next(!this.isOpen$.value);
+  }
+
+  openMenu() {
+    this.isOpen$.next(true);
+  }
+
+  closeMenu() {
+    this.isOpen$.next(false);
+  }
+
+  manageBody() {
+    if (this.isMenuOpen && window) {
+      window.document.body.style.position = 'fixed';
+    } else {
+      window.document.body.style.position = 'relative';
+    }
+  }
 
 }
