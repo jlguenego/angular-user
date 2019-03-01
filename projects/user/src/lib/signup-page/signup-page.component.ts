8a7e98c841b9705ff881659aa50040c133d99987
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-03-01 18:11:58 +0100

    signup title

--- projects/user/src/lib/signup-page/signup-page.component.ts
+++ projects/user/src/lib/signup-page/signup-page.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { PageComponent } from 'projects/layout/src/lib/dialog.service';
 
 @Component({
   selector: 'user-signup-page',
   templateUrl: './signup-page.component.html',
   styleUrls: ['./signup-page.component.scss']
 })
-export class SignupPageComponent implements OnInit {
+export class SignupPageComponent implements OnInit, PageComponent {
+
+  title = "Sign up";
 
   constructor() { }
 
