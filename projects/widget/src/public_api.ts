eaa1893ca06b7d183883a5d5f447cab69760042d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-03-03 18:46:43 +0100

    widget

--- projects/widget/src/public_api.ts
+++ projects/widget/src/public_api.ts
@@ -3,3 +3,12 @@
  */
 
 export * from './lib/widget.module';
+
+export * from './lib/checkbox/checkbox.component';
+export * from './lib/divider/divider.component';
+export * from './lib/password/password.component';
+export * from './lib/password-check/password-check.component';
+export * from './lib/popup/popup.component';
+
+export * from './lib/password-check.service';
+export * from './lib/AbstractValueAccessor';
