5a726a90c49972abc3782a958c0e96577b8e863f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-03-04 09:11:40 +0100

    user export

--- projects/user/src/public_api.ts
+++ projects/user/src/public_api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of user
  */
 
+export * from './lib/user.module';
 export * from './lib/user.service';
 export * from './lib/utilities/utilities.component';
-export * from './lib/user.module';
+export * from './lib/mobile-menu-icon/mobile-menu-icon.component';
