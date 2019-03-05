598afbd1c578f3165275d7863d2093d0e79f8cf3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-03-05 18:14:34 +0100

    activation ongoing

--- projects/user/src/lib/pages/activation-page/activation-page.component.ts
+++ projects/user/src/lib/pages/activation-page/activation-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../user.service';
 
 @Component({
   selector: 'user-activation-page',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivationPageComponent implements OnInit {
   title = "Activate your account";
-  constructor() { }
+  constructor(public user: UserService) { }
 
   ngOnInit() {
   }
