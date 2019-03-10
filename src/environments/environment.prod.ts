034c12f28fde2699fe9378b826210fd79d04a725
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-03-10 14:43:56 +0100

    firebase deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD9Ym1lWrk1iVjwqin8TOrqQBT6yWms-4A",
+    authDomain: "angular-user-test.firebaseapp.com",
+    databaseURL: "https://angular-user-test.firebaseio.com",
+    projectId: "angular-user-test",
+    storageBucket: "angular-user-test.appspot.com",
+    messagingSenderId: "1068906809055"
+  }
 };
