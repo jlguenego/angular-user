cc118ffd8adbddeecd588b6259d6f8219e37815a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-04-14 11:42:18 +0200

    test directive

--- projects/layout/src/lib/widget/anchor.directive.spec.ts
+++ projects/layout/src/lib/widget/anchor.directive.spec.ts
@@ -1,8 +1,23 @@
 import { AnchorDirective } from './anchor.directive';
+import { async, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+
+@Component({
+  template: `<p>Test</p>`,
+})
+class TestComponent { }
 
 describe('AnchorDirective', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestComponent, AnchorDirective]
+    }).compileComponents();
+  }));
+
   it('should create an instance', () => {
-    const directive = new AnchorDirective();
-    expect(directive).toBeTruthy();
+    const fixture = TestBed.createComponent(TestComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
   });
+
 });
