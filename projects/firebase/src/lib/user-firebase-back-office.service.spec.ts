5a646221191823eecb01d967bf3c2df359c4d48a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-04-14 12:08:42 +0200

    ng test firebase ok

--- projects/firebase/src/lib/user-firebase-back-office.service.spec.ts
+++ projects/firebase/src/lib/user-firebase-back-office.service.spec.ts
@@ -1,9 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserFirebaseBackOfficeService } from './user-firebase-back-office.service';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
 
 describe('UserFirebaseBackOfficeService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      AngularFireModule.initializeApp({
+        apiKey: '<your-key>',
+        authDomain: '<your-project-authdomain>',
+        databaseURL: '<your-database-URL>',
+        projectId: '<your-project-id>',
+        storageBucket: '<your-storage-bucket>',
+        messagingSenderId: '<your-messaging-sender-id>'
+      }),
+      AngularFireAuthModule,
+    ],
+    providers: [UserFirebaseBackOfficeService]
+  }));
 
   it('should be created', () => {
     const service: UserFirebaseBackOfficeService = TestBed.get(UserFirebaseBackOfficeService);
